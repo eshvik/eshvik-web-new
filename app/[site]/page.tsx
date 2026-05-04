d956e69a35d4063a54b4591251ac3688a2b2a94b
Dr M <eshvik@users.noreply.github.com>
2026-05-04 22:26:42 +0530

    connect supabase

--- app/[site]/page.tsx
+++ app/[site]/page.tsx
@@ -1,3 +1,19 @@
-export default function Site({ params }: { params: { site: string } }) {
-  return <h1>Subdomain: {params.site}</h1>
+import { supabase } from "@/lib/supabase";
+
+export default async function SitePage({ params }: any) {
+  const { site } = params;
+
+  const { data } = await supabase
+    .from("sites")
+    .select("*")
+    .eq("name", site)
+    .single();
+
+  if (!data) {
+    return <h1>❌ Site not found</h1>;
+  }
+
+  return (
+    <div dangerouslySetInnerHTML={{ __html: data.content }} />
+  );
 }
---
+++ lib/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+"https://ajhuzmwfygflppnmzzxw.supabase.co/",
+"sb_publishable_PNcQchuKerVK2FwmPUVW8Q_tp1z0zX0"
+);
